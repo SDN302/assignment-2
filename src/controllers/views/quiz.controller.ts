413f01f33011ec041483d6e14f988e0162116ef2
Minh Vương <hardingadonis@gmail.com>
2024-09-11 11:29:04 +0700

    add: implement display page for quiz

--- src/controllers/views/quiz.controller.ts
+++ src/controllers/views/quiz.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { IQuiz } from '../../models/quiz.model';
 import { server } from '../../config/config';
 import axios from 'axios';
+import { IQuestion } from '../../models/question.model';
 
 export const allQuizzes = async (req: Request, res: Response) => {
 	const response = await axios.get<IQuiz[]>(
@@ -15,4 +16,25 @@ export const allQuizzes = async (req: Request, res: Response) => {
 
 export const newQuiz = async (req: Request, res: Response) => {
 	res.render('quizzes/newQuiz');
+};
+
+export const displayQuiz = async (req: Request, res: Response) => {
+	const { id } = req.query;
+
+	const response = await axios.get<IQuiz>(
+		`${server.schema}://${server.host}:${server.port}/api/quizzes/${id}`,
+	);
+
+	const quiz = response.data;
+
+	const questionPromises = quiz.questions.map(async (questionId) => {
+		const questionResponse = await axios.get<IQuestion>(
+			`${server.schema}://${server.host}:${server.port}/api/questions/${questionId}`,
+		);
+		return questionResponse.data;
+	});
+
+	const questions = await Promise.all(questionPromises);
+
+	res.render('quizzes/displayQuiz', { quiz: quiz, questions: questions });
 };
--- src/routes/views/quiz.route.ts
+++ src/routes/views/quiz.route.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 
-import { allQuizzes, newQuiz } from '../../controllers/views/quiz.controller';
+import {
+	allQuizzes,
+	displayQuiz,
+	newQuiz,
+} from '../../controllers/views/quiz.controller';
 
 const router = express.Router();
 
 router.get('/', allQuizzes);
+
+router.get('/display', displayQuiz);
 
 router.get('/new', newQuiz);
 
